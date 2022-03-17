e96900893ac1ff70bd5b55306ed6fc5e6ac17668
Tawfiq <tawfiq.mouttalif@diji.fr>
2022-03-17 17:25:12 +0100

    swipe question

---
+++ assets/js/swiper.js
@@ -0,0 +1,6 @@
+new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,3 +1,6 @@
+//Title
+document.getElementById("theme").innerHTML = "Histoire"
+
 //Date today 
 date = new Date();
 year = date.getFullYear();
@@ -5,13 +8,13 @@ month = date.getMonth() + 1;
 day = date.getDate();
 document.getElementById("today").innerHTML = month + "/" + day + "/" + year;
 
-//Hide menu
+//Hide PopUp
 let nDialog = document.getElementById("dialog-note")
 let closeDialog = document.getElementById("popup-close")
 
 nDialog.style.display = 'none'
 
-function isHideMenu(isHide){
+function isHidePopUp(isHide){
     if(isHide){
         nDialog.style.display = 'none'
     }else{
@@ -19,28 +22,64 @@ function isHideMenu(isHide){
     }
 };
 
-function getResponse(){
-    var ele = document.getElementsByName("pays"); //Tableau name
-    console.log(ele.length);
-     
-    for(i = 0; i < ele.length; i++) {
-        console.log(ele[i].value);
-        
-        if(ele[i].checked){
-            document.getElementById("aze").innerHTML = calculTauxReussite() +"%";                
+//Content Question
+function launchValidateAnswer(nbAnswer){
+    var tab = new Array(nbAnswer);
+    var nbRespEchec = 0;
+
+    tab[0] = checkAnswer("date","b");
+    tab[1] = checkAnswer("pays","c");
+    tab[2] = checkAnswer("ville","a");
+    tab[3] = checkAnswer("album","d");
+    
+    for(let i = 0 ; i < tab.length ; i++){
+       if(tab[i] == false){
+           nbRespEchec++
+       }
+    }
+    console.log("nbRespEchec "+nbRespEchec +" | " +"nbAnswer "+tab.length)
+    let tauxReussitePrc = calculTauxReussite(nbRespEchec ,tab.length)
+
+    document.getElementById("taux").innerHTML = tauxReussitePrc +"%"; 
+    moveProgress(tauxReussitePrc);
+}
+
+function checkAnswer(name,respValid){
+    var ele = document.getElementsByName(name); //Tableau
+    for(i = 0; i < ele.length; i++) {        
+        let resp =ele[i]
+        if(resp.checked){
+            if(resp.value == respValid){
+                return true
+            }
         }
+    }
+    return false
+}
+
+function moveProgress(tauxReussite) {
+    var start = 0;
+    console.log("moveProgress start "+start)
+
+    if (start == 0) {
+        start = 1;
+        let progress = document.getElementById("progress")
+        var width = 1;
+        var idInterval= setInterval(moveInterval, 12); 
+       
+        function moveInterval(){
+            if (width >= tauxReussite) {
+                clearInterval(idInterval);
+            } else {
+                width++; 
+                progress.value = width; 
+                console.log("moveProgress width "+width)
+            }
+        }  
     }
 }
 
-//Listenner
-document.getElementById("buttonValid").addEventListener("click", getResponse);
-
-//Update progress 
-function calculTauxReussite() {
-    let nbRespEchec = 2;
-    let nbQuestion = 3;
-    return (nbRespEchec * nbQuestion)/100;
+function calculTauxReussite(nbRespEchec,nbQuestion) {
+    let tauxEchec = (nbRespEchec/ nbQuestion)*100
+    return (100 - tauxEchec).toFixed(2);
 }
-
-let progress = document.getElementById("progress").value = "70"
-//var textProgress = document.getElementById("aze").innerText = calculTauxReussite() +"%"
